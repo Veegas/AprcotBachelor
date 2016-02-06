fd9f93d7e8e8b565cd3a53ebde27eacf748662a6
Mina Boutros <m.boutros@aprcot.com>
2016-02-06 21:33:11 +0200

    guc-prep

--- static/app/application/contactus/controllers/retrieveContactusAppController.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    'use strict';
-    var applicationModule = angular.module('aprcotApp.application.controllers');
-    applicationModule.controller('retrieveContactusAppController', function($scope, FormService,
-        campaignId, settings) {
-        $scope.form = settings;
-        $scope.form.campaign_id = campaignId;
-    });
-})();
